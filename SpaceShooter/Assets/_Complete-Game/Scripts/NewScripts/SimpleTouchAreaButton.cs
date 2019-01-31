6546240987dcb21c8f5f7a1c3b146b4207e6c7b8
Unknown <hasnatabdullah79@gmail.com>
2019-02-01 01:01:51 +0600

    Move-shoot
    Added touch functionality. 
    Can move and shoot in mobile now

---
+++ SpaceShooter/Assets/_Complete-Game/Scripts/NewScripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SimpleTouchAreaButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+
+    private bool touched;
+    private int pointerID;
+    private bool canfire;
+
+    private void Awake()
+    {
+        
+        touched = false;
+
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (!touched)
+        {
+            touched = true;
+            pointerID = data.pointerId;
+            canfire = true;
+
+        }
+    }
+ 
+
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        if (data.pointerId == pointerID)
+        {
+            canfire = false;
+            touched = false;
+        }
+    }
+
+    public bool CanFire()
+    {
+        return canfire;
+    }
+}
