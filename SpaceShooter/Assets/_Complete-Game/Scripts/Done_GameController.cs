c08ef0be7e6b5bfac0566e42a97c6c45970802a6
Unknown <hasnatabdullah79@gmail.com>
2019-02-01 13:09:38 +0600

    Reset-level-MovementZoneAnim
    till movement zone animation creation

--- SpaceShooter/Assets/_Complete-Game/Scripts/Done_GameController.cs
+++ SpaceShooter/Assets/_Complete-Game/Scripts/Done_GameController.cs
@@ -3,18 +3,24 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class Done_GameController : MonoBehaviour
 {
+    public Animator animator;
     public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
+    //Adding Levels
+    public Text levelText;
+    private int level;
 
     public Text scoreText;
-    public Text restartText;
+    //public Text restartText;
     public Text gameOverText;
+    public GameObject restartButton;
 
     private bool gameOver;
     private bool restart;
@@ -22,25 +28,22 @@ public class Done_GameController : MonoBehaviour
 
     void Start()
     {
+        level = 1;
         gameOver = false;
         restart = false;
-        restartText.text = "";
+        //restartText.text = "";
+        restartButton.SetActive(false);
         gameOverText.text = "";
+        levelText.text = "Level " + level;
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
     }
 
     void Update()
-    {
-        if (restart)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
+ {
+        levelText.text = "level " + level;
+ }
 
     IEnumerator SpawnWaves()
     {
@@ -49,20 +52,31 @@ public class Done_GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                animator.SetInteger("spawn_loop_counter", i);
                 GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
-            }
-            yield return new WaitForSeconds(waveWait);
+                
 
+            }
+
+            yield return new WaitForSeconds(waveWait);
+           
             if (gameOver)
             {
-                restartText.text = "Press 'R' for Restart";
+                //  restartText.text = "Press 'R' for Restart";
+                restartButton.SetActive(true);
                 restart = true;
                 break;
+            }else
+            {
+                hazardCount += 5;
+                level++;
             }
+
+
         }
     }
 
@@ -81,5 +95,10 @@ public class Done_GameController : MonoBehaviour
     {
         gameOverText.text = "Game Over!";
         gameOver = true;
+
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
