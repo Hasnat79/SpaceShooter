cdb2c4e394f20e07e20cf46baefaa3b0befd6f04
Unknown <hasnatabdullah79@gmail.com>
2019-02-01 21:42:16 +0600

    GameOverUI
    added fonts and animation in gameover UI
    and changed the restart button position

--- SpaceShooter/Assets/_Complete-Game/Scripts/Done_GameController.cs
+++ SpaceShooter/Assets/_Complete-Game/Scripts/Done_GameController.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 
 public class Done_GameController : MonoBehaviour
 {
-    public Animator animator;
-   public Animator animator2;
+    
+
+    public Animator moveZoneanimator;
+    public Animator fireZoneanimator;
+    //gameover text animator
+    public Animator game_text_animator;
+    public Animator over_text_animator;
+   // bool gameOver_boolean;
     public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
@@ -17,25 +23,31 @@ public class Done_GameController : MonoBehaviour
     //Adding Levels
     public Text levelText;
     private int level;
+    //Adding Highest Score
+    public Text highestScoreText;
+    private int highestScore;
 
-    public Text scoreText;
+    
     //public Text restartText;
-    public Text gameOverText;
+    //public Text gameOverText;
     public GameObject restartButton;
 
     private bool gameOver;
     private bool restart;
+    public Text scoreText;
     private int score;
-
+    
     void Start()
     {
+        //gameOver_boolean = false;
         level = 1;
         gameOver = false;
         restart = false;
         //restartText.text = "";
         restartButton.SetActive(false);
-        gameOverText.text = "";
+        //gameOverText.text = "";
         levelText.text = "Level " + level;
+        highestScoreText.text = "";
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
@@ -44,6 +56,10 @@ public class Done_GameController : MonoBehaviour
     void Update()
  {
         levelText.text = "level " + level;
+  //      if(score > highestScore)
+  //    {
+  //      highestScore = score;
+  //    }
  }
 
     IEnumerator SpawnWaves()
@@ -53,38 +69,43 @@ public class Done_GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
-                animator.SetInteger("spawn_loop_counter", i);
-               animator2.SetInteger("Spawn_loop_counter2", i);
+                moveZoneanimator.SetInteger("spawn_loop_counter", i);
+                fireZoneanimator.SetInteger("Spawn_loop_counter2", i);
                 GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
-                
-
+                if (gameOver)
+                {
+                    //  restartText.text = "Press 'R' for Restart";
+                    //gameOver_boolean = true;
+                    game_text_animator.SetBool("gameOver1", gameOver);
+                    over_text_animator.SetBool("gameOver2",gameOver);
+                    restartButton.SetActive(true);
+                    restart = true;
+                    highestScoreText.text = "HIGH SCORE \n"+PlayerPrefs.GetInt("highestScore",0).ToString();//Don't forget to reset by button or anything :3
+                    break;
+                }
             }
-
+            
+            if (!gameOver)
+            {                            
+                    hazardCount += 5;
+                    level++;
+            }
             yield return new WaitForSeconds(waveWait);
-           
-            if (gameOver)
-            {
-                //  restartText.text = "Press 'R' for Restart";
-                restartButton.SetActive(true);
-                restart = true;
-                break;
-            }else
-            {
-                hazardCount += 5;
-                level++;
-            }
-
-
+          
         }
     }
 
     public void AddScore(int newScoreValue)
     {
         score += newScoreValue;
+        if (score > PlayerPrefs.GetInt("highestScore", 0))
+        {
+            PlayerPrefs.SetInt("highestScore", score);
+        }
         UpdateScore();
     }
 
@@ -93,9 +114,10 @@ public class Done_GameController : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+
     public void GameOver()
     {
-        gameOverText.text = "Game Over!";
+        //gameOverText.text = "Game Over!";
         gameOver = true;
 
     }
